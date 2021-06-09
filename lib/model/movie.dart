3059c13415011e4f6de933933396041332e34bda
unknown <huytung139@gmail.com>
2021-06-09 08:37:26 +0700

    building detail screen

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -3,7 +3,10 @@ class Movie {
         Movie(
             "AVARTAR",
             "This is Avartar",
-            "https://th.bing.com/th/id/OIP.qoZ841wL3k1EZV7u1uD1YAHaK7?pid=ImgDet&rs=1",
+            [
+              "https://th.bing.com/th/id/OIP.qoZ841wL3k1EZV7u1uD1YAHaK7?pid=ImgDet&rs=1",
+              "https://static2.srcdn.com/wordpress/wp-content/uploads/2018/07/Neytiri-in-Avatar.jpg"
+            ],
             "4.5",
             "780",
             "120",
@@ -14,7 +17,10 @@ class Movie {
         Movie(
             "Iron Man 1",
             "This is Iron Man",
-            "https://th.bing.com/th/id/OIP.suSW935mrFMxGHxXCm26cwHaK-?pid=ImgDet&rs=1",
+            [
+              "https://th.bing.com/th/id/OIP.suSW935mrFMxGHxXCm26cwHaK-?pid=ImgDet&rs=1",
+              "https://th.bing.com/th/id/R758423ca3f66d228260f7aabb4cd19e0?rik=KPjyKBR%2bZH0jCg&riu=http%3a%2f%2fwww.cbr.com%2fwp-content%2fuploads%2f2017%2f08%2firon-man-mark-1.jpg&ehk=z75hElEtcj8ToOovfhiPLO%2fnqOK%2bAYDpy0%2fmO5LHTK8%3d&risl=&pid=ImgRaw"
+            ],
             "4.5",
             "780",
             "126",
@@ -25,7 +31,10 @@ class Movie {
         Movie(
             "AVARTAR",
             "This is Avartar",
-            "https://th.bing.com/th/id/OIP.qoZ841wL3k1EZV7u1uD1YAHaK7?pid=ImgDet&rs=1",
+            [
+              "https://th.bing.com/th/id/OIP.qoZ841wL3k1EZV7u1uD1YAHaK7?pid=ImgDet&rs=1",
+              "https://static2.srcdn.com/wordpress/wp-content/uploads/2018/07/Neytiri-in-Avatar.jpg"
+            ],
             "4.5",
             "780",
             "120",
@@ -36,7 +45,10 @@ class Movie {
         Movie(
             "Iron Man 1",
             "This is Iron Man",
-            "https://th.bing.com/th/id/OIP.suSW935mrFMxGHxXCm26cwHaK-?pid=ImgDet&rs=1",
+            [
+              "https://th.bing.com/th/id/OIP.suSW935mrFMxGHxXCm26cwHaK-?pid=ImgDet&rs=1",
+              "https://th.bing.com/th/id/R758423ca3f66d228260f7aabb4cd19e0?rik=KPjyKBR%2bZH0jCg&riu=http%3a%2f%2fwww.cbr.com%2fwp-content%2fuploads%2f2017%2f08%2firon-man-mark-1.jpg&ehk=z75hElEtcj8ToOovfhiPLO%2fnqOK%2bAYDpy0%2fmO5LHTK8%3d&risl=&pid=ImgRaw"
+            ],
             "4.5",
             "780",
             "126",
@@ -47,7 +59,10 @@ class Movie {
         Movie(
             "AVARTAR",
             "This is Avartar",
-            "https://th.bing.com/th/id/OIP.qoZ841wL3k1EZV7u1uD1YAHaK7?pid=ImgDet&rs=1",
+            [
+              "https://th.bing.com/th/id/OIP.qoZ841wL3k1EZV7u1uD1YAHaK7?pid=ImgDet&rs=1",
+              "https://static2.srcdn.com/wordpress/wp-content/uploads/2018/07/Neytiri-in-Avatar.jpg"
+            ],
             "4.5",
             "780",
             "120",
@@ -58,7 +73,10 @@ class Movie {
         Movie(
             "Iron Man 1",
             "This is Iron Man",
-            "https://th.bing.com/th/id/OIP.suSW935mrFMxGHxXCm26cwHaK-?pid=ImgDet&rs=1",
+            [
+              "https://th.bing.com/th/id/OIP.suSW935mrFMxGHxXCm26cwHaK-?pid=ImgDet&rs=1",
+              "https://th.bing.com/th/id/R758423ca3f66d228260f7aabb4cd19e0?rik=KPjyKBR%2bZH0jCg&riu=http%3a%2f%2fwww.cbr.com%2fwp-content%2fuploads%2f2017%2f08%2firon-man-mark-1.jpg&ehk=z75hElEtcj8ToOovfhiPLO%2fnqOK%2bAYDpy0%2fmO5LHTK8%3d&risl=&pid=ImgRaw"
+            ],
             "4.5",
             "780",
             "126",
@@ -70,7 +88,7 @@ class Movie {
 
   String title;
   String subtitle;
-  String avartar;
+  List image;
   String rate;
   String numRate;
   String time;
@@ -79,6 +97,6 @@ class Movie {
   String genre;
   String type;
 
-  Movie(this.title, this.subtitle, this.avartar, this.rate, this.numRate,
+  Movie(this.title, this.subtitle, this.image, this.rate, this.numRate,
       this.time, this.released, this.director, this.genre, this.type);
 }
